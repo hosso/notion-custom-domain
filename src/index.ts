f7d1fe94035bf33631f17fddb6ac097d8bfd0645
hosso <hosso@users.noreply.github.com>
2024-03-10 05:18:44 +0900

    Optimize requests from page

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,43 @@ const ga = GA_MEASUREMENT_ID
 </script>`
   : '';
 
+const customScript = `<script>
+  (function (){
+    const replacedUrl = (url) => {
+      const [,domain] = /^https?:\\/\\/([^\\/]*)/.exec(url) || [,''];
+      if (
+        domain.endsWith("notion.so") && !domain.endsWith("msgstore.www.notion.so") ||
+        domain.endsWith("splunkcloud.com") ||
+        domain.endsWith("statsigapi.net")
+      ) {
+        console.info("[NCD]", "Suppress request:", url);
+        return url.replace(/^.*:(.*)\\/\\//, "/200/$1");
+      }
+      return url;
+    }
+
+    fetch = new Proxy(fetch, {
+      apply: function(target, that, args) {
+        args[0] = replacedUrl(args[0]);
+        return Reflect.apply(...arguments);
+      }
+    })
+
+    XMLHttpRequest = new Proxy(XMLHttpRequest, {
+      construct: function(target, args) {
+        const xhr = new target(...args);
+        xhr.open = new Proxy(xhr.open, {
+          apply: function(target, that, args) {
+            args[1] = replacedUrl(args[1]);
+            return Reflect.apply(...arguments);
+          }
+        });
+        return xhr;
+      }
+    })
+  })();
+</script>`;
+
 const customStyle = `<style> 
   .notion-topbar > div > div:nth-last-child(1), .notion-topbar > div > div:nth-last-child(2) { 
     display:none !important; 
@@ -43,6 +80,20 @@ const app = express();
 
 app.use(
   proxy(pageDomain, {
+    filter: (req, res) => {
+      // Pseudo endpoint returning 200
+      if (/^\/200\/?/.test(req.url)) {
+        if (req.url.startsWith('/200/www.notion.so/api/v3/')) {
+          res.send('success');
+        } else if (req.url.startsWith('/200/exp.notion.so/v1/')) {
+          res.json({ success: true });
+        } else {
+          res.end();
+        }
+        return false;
+      }
+      return true;
+    },
     proxyReqPathResolver: (req) => {
       // Replace '/' with `/${pageId}`
       return req.url.replace(/\/(\?|$)/, `/${pageId}$1`);
@@ -54,8 +105,8 @@ app.use(
         // "; Domain=notion.site;' -> '; Domain=mydomain.com;"
         headers['set-cookie'] = cookies.map((cookie) =>
           cookie.replace(
-            /((?:^|; )Domain=)((?:[^.]+\.)?notion\.site)(;|$)/gi,
-            `$1${userReq.hostname}$3`,
+            /((?:^|; )Domain=)(?:[^.]+\.)?notion\.site(;|$)/gi,
+            `$1${userReq.hostname}$2`,
           ),
         );
       }
@@ -71,21 +122,58 @@ app.use(
       return headers;
     },
     userResDecorator: (_proxyRes, proxyResData, userReq) => {
+      if (/^\/image[s]?\//.test(userReq.url)) {
+        return proxyResData;
+      }
+
+      let data = proxyResData.toString();
+
+      // For investigation
+      const keywords: string[] = [
+        // 'teV1',
+        // 'aif.notion.so',
+        // 'exp.notion.so',
+        // 'msgstore.www.notion.so',
+        // 'primus',
+        // 'widget.intercom.io',
+        // 'ingest.sentry.io',
+        // 'envelope',
+        // 'dsn',
+        // 'splunkcloud.com',
+        // 'statsigapi.net',
+      ];
+      const found = keywords.reduce(
+        (acc: string[], keyword) =>
+          data.includes(keyword) ? [...acc, keyword] : acc,
+        [],
+      );
+      if (found.length > 0) {
+        console.log('[DEBUG]', userReq.url, found);
+      }
+
       if (/^\/_assets\/[^/]*\.js$/.test(userReq.url)) {
-        return proxyResData
-          .toString()
+        data = data
           .replace(/^/, ncd)
           .replace(/window\.location\.href(?=[^=]|={2,})/g, 'ncd.href()') // Exclude 'window.locaton.href=' but not 'window.locaton.href=='
           .replace(/window\.history\.(pushState|replaceState)/g, 'ncd.$1');
-      } else if (/^\/image[s]?\//.test(userReq.url)) {
-        return proxyResData;
       } else {
         // Assume HTML
-        return proxyResData
-          .toString()
+        data = data
           .replace('</body>', `${ga}</body>`)
-          .replace('</head>', `${customStyle}</head>`);
+          .replace('</head>', `${customScript}${customStyle}</head>`);
       }
+
+      data = data
+        // https://aif.notion.so/**      -> /200/aif.notion.so/**
+        // https://widget.intercom.io/** -> /200/widget.intercom.io/**
+        .replace(
+          /https:\/\/((aif\.notion\.so|widget\.intercom\.io)\/?[^"`]*)/g,
+          `/200/$1`,
+        )
+        // Skip Sentry.init()
+        .replace(/\w+\.init\({dsn:/, 'return;$&');
+
+      return data;
     },
   }),
 );
